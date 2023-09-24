0f1862297e654d0fcc89b06430c03be69706ea65
Karysqa <85331340+Karysqa12xc@users.noreply.github.com>
2023-09-24 17:37:48 +0700

    Update: Done control flow and functions

--- dart_paractive/bin/dart_paractive.dart
+++ dart_paractive/bin/dart_paractive.dart
@@ -1,18 +1,24 @@
 import 'dart:collection';
+import 'dart:io';
 
-enum Fruits { Apple, Pear, Grapes, Banana, Orange }
-
+bool checkEven(int value) => value % 2 == 0;
 void main(List<String> arguments) {
-  Fruits liked = Fruits.Apple;
-  var disliked = Fruits.Banana;
-
-  print(liked.toString());
-  print(disliked.toString());
-  num? value = 5;
-  int otherValue = value as int;
-  print(otherValue);
-
-  double? pi = 3.14;
-  final round1 = pi.round();
-  print(round1);
+  print(test());
+  //* Gán hàm cho một biến
+  bool Function(int) checker = checkEven;
+  //* Anonymous function
+  bool Function(int) isEven = (int value) => value % 2 == 0;
+  print(isEven(19));
+  final numbers = [2, 4, 6, 6, 10];
+  numbers.forEach(showNumber);
 }
+
+//* Nếu không có kiểu trả về trong hàm thì dart sẽ mặc định
+//* gán kiểu trả về là dynamic
+test() {
+  int i = 7;
+  return i;
+}
+
+void test2([int? a, int? b]) {}
+void showNumber(int value) => print(value);
