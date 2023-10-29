2cddec58dc85952cefc9060fed7cee88d26b898d
Karysqa <85331340+Karysqa12xc@users.noreply.github.com>
2023-10-29 17:29:11 +0700

    SOLID: Open Close Principle

--- dart_paractive/bin/main.dart
+++ dart_paractive/bin/main.dart
@@ -1,8 +1,7 @@
+import 'solid_learn.dart';
+
 void main(List<String> args) {
-  
+  Circle c = Circle(10);
+  AreaCalculator a = AreaCalculator();
+  print(a.calculator(c));
 }
-
-
-
-
-
--- dart_paractive/bin/solid_learn.dart
+++ dart_paractive/bin/solid_learn.dart
@@ -1,50 +1,76 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-
-
 abstract class Shape {
   double area();
 }
 
-class Square  {
- 
+abstract class Area {
+  double computeArea();
 }
 
-class Rectangle extends Shape{
+class Square implements Area {
+  @override
+  double computeArea() {
+    // TODO: implement computeArea
+    throw UnimplementedError();
+  }
+}
+
+class Rectangle implements Area {
   final double width;
   final double height;
   Rectangle(
     this.width,
     this.height,
   );
-  
+
   @override
-  double area() {
-    // TODO: implement area
+  double computeArea() {
+    // TODO: implement computeArea
     throw UnimplementedError();
   }
 }
 
-class Circle extends Shape{
+class Circle implements Area {
   final double radius;
   Circle(this.radius);
   double get PI => 3.14;
-  
+
   @override
-  double area() {
-    // TODO: implement area
+  double computeArea() {
+    return radius * radius * PI;
+  }
+}
+
+class Triangle implements Area {
+  @override
+  double computeArea() {
+    // TODO: implement computeArea
     throw UnimplementedError();
   }
 }
-// class AreaCalculator {
-//   double calculator(Object shape){
-//     if(shape is Rectangle){
-//       return shape.width * shape.height;
-//     }else{
-//       final c = shape as Circle;
-//       return c.radius * c.radius * c.PI;
-//     }
-//   }
-// }
+
+class Rhombus implements Area {
+  @override
+  double computeArea() {
+    // TODO: implement computeArea
+    throw UnimplementedError();
+  }
+}
+
+class Trapezoid implements Area {
+  @override
+  double computeArea() {
+    // TODO: implement computeArea
+    throw UnimplementedError();
+  }
+}
+
+class AreaCalculator {
+  double calculator(Area shape) {
+    return shape.computeArea();
+  }
+}
+
 class ShapePainter {}
 
 class ShapesOnline {}
